d47ea01eb280882dc476897919b97af936269b95
JonGudenzi <jongudenzi@gmail.com>
2021-03-24 21:25:50 -0400

    correction to navbar

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header/Header';
-
-// import About from './components/About/About';
-// import Work from './components/Work/Work';
-// import Contact from './components/Contact/Contact';
-
+import Home from './components/Home/Home';
 
 ReactDOM.render(
     <React.StrictMode>
-      <Header />
-      {/* <About />
-      <Work />
-      <Contact /> */}
+        <Home />
     </React.StrictMode>,
     document.getElementById('root')
-  );
-  
+);
+
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  
